f6059d111043329fe26ebd0a8089be3729bc4cf1
User <User@DESKTOP-ESP2JT1>
2022-11-03 16:29:33 +0200

    Add project files.

---
+++ 03. Time + 15 Minutes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _03._Time___15_Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            int time = (hours * 60 + minutes) + 15;
+            int hoursform = time / 60;
+            int minutesform = time % 60;
+
+            if (hoursform == 24)
+            {
+                Console.WriteLine($"0:{minutesform:d2}");
+            }
+            else
+            {
+                Console.WriteLine($"{hoursform}:{minutesform:d2}");
+            }
+            
+            {
+
+            }
+
+
+        }
+    }
+}
